ee9dae48fe6c7b613b9dd0c74bf134e60ca8c2a2
Ivan Kopylov <ivan.kopylove@gmail.com>
2014-08-08 14:01:35 +0400

    Теперь командой !stat также выводится количество жалоб, отправленных ботом автоматически

--- La_cryogenie/commands/Stat.cs
+++ La_cryogenie/commands/Stat.cs
@@ -39,19 +39,48 @@ namespace La_cryogenie
             long now = Utilities.getCurrentUnixTime();
             long from = now - (86400 * days);
 
+            SkypeSingleton.Instance.sendChatMessage(msg.ChatName, string.Format("Фишинговых ссылок и мошеннических файлов, отправленных за последние {0} дней:", days.ToString()));
+
+
+            #region autoreport by bot
+            DataTable phishingUrlsReportedByBot = Sqlite.executeSearch(string.Format("SELECT * FROM [links] WHERE (category = 'phishing_page' OR category = 'malware') AND last_report_to_hoster > {0} AND reporter_to_hoster ='selfconsciousness' ORDER BY last_report_to_hoster DESC;", from));
+            if (phishingUrlsReportedByBot.Rows.Count == 0)
+            {
+                SkypeSingleton.Instance.sendChatMessage(msg.ChatName, string.Format(" • За этот период жалоб АВТОМАТИЧЕСКИ хостеру не отправлялось."));
+            }
+            else
+            {
+                int sentByBot = new int();
+
+                SkypeSingleton.Instance.sendChatMessage(msg.ChatName, string.Format(" • Автоматически: {0}", phishingUrlsReportedByBot.Rows.Count.ToString()));
+
+                string result = "Возможно, все ссылки не уместятся в одно сообщение.\nпроект || дата || оригинальная ссылка" + Environment.NewLine;
+                foreach (DataRow row in phishingUrlsReportedByBot.Rows)
+                {
+                    sentByBot++;
+                    result += string.Format
+                        (
+                        "|| {0} || {1} || {2} || http://{3} \n",
+                        sentByBot, row.Field<string>("game_projects"), Utilities.convertFromUnixTimestamp(row.Field<long>("last_report_to_hoster")).ToShortDateString(), row.Field<string>("original_url")
+                        );
+                }
+                //SkypeSingleton.Instance.sendChatMessage(msg.ChatName, result);
+            }
+            #endregion
+
             if (2 == commandArguments.Length)
             {
                 DataTable phishingUrlsAll = Sqlite.executeSearch(string.Format("SELECT * FROM [links] WHERE (category = 'phishing_page' OR category = 'malware') AND last_report_to_hoster > {0} ORDER BY last_report_to_hoster DESC;", from));
                 if (phishingUrlsAll.Rows.Count == 0)
                 {
-                    SkypeSingleton.Instance.sendChatMessage(msg.ChatName, string.Format("Пусто"));
+                    SkypeSingleton.Instance.sendChatMessage(msg.ChatName, string.Format("{0}, я ничего не нашла.", msg.Sender.FullName));
                     return;
                 }
                 else
                 {
                     int count = 0;
 
-                    SkypeSingleton.Instance.sendChatMessage(msg.ChatName, string.Format("Всего фишинговых ссылок и мошеннических файлов за последние {0} дней: {1}", days.ToString(), phishingUrlsAll.Rows.Count.ToString()));
+                    SkypeSingleton.Instance.sendChatMessage(msg.ChatName, string.Format(" • Всего: {1}", days.ToString(), phishingUrlsAll.Rows.Count.ToString()));
 
 
                     string result = "Возможно, все ссылки не уместятся в одно сообщение.\nпроект || дата || оригинальная ссылка" + Environment.NewLine;
@@ -74,14 +103,13 @@ namespace La_cryogenie
                 DataTable phishingUrlsByReporterToHoster = Sqlite.executeSearch(string.Format("SELECT * FROM [links] WHERE (category = 'phishing_page' OR category = 'malware') AND last_report_to_hoster > {0} AND reporter_to_hoster ='{1}' ORDER BY last_report_to_hoster DESC;", from, commandArguments[2]));
                 if (phishingUrlsByReporterToHoster.Rows.Count == 0)
                 {
-                    SkypeSingleton.Instance.sendChatMessage(msg.ChatName, string.Format("Пусто"));
-                    return;
+                    SkypeSingleton.Instance.sendChatMessage(msg.ChatName, string.Format(" • Отправленных {0}: нет", commandArguments[2]));
                 }
                 else
                 {
                     int count = 0;
 
-                    SkypeSingleton.Instance.sendChatMessage(msg.ChatName, string.Format("Фишинговых ссылок и мошеннических файлов, отправленных {0} за последние {1} дней: {2}", commandArguments[2], days.ToString(), phishingUrlsByReporterToHoster.Rows.Count.ToString()));
+                    SkypeSingleton.Instance.sendChatMessage(msg.ChatName, string.Format(" • Отправленных {0}: {1}", commandArguments[2], phishingUrlsByReporterToHoster.Rows.Count.ToString()));
 
                     string result = "Возможно, все ссылки не уместятся в одно сообщение.\nпроект || дата || оригинальная ссылка" + Environment.NewLine;
                     foreach (DataRow row in phishingUrlsByReporterToHoster.Rows)
@@ -94,9 +122,11 @@ namespace La_cryogenie
                             );
                     }
                     SkypeSingleton.Instance.sendChatMessage(msg.ChatName, result);
-                    return;
                 }
+
             }
+
+
         }
     }
 }
