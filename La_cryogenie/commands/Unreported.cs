6219d83415f1fc59e18c1d3d1e4ec3649d61ada4
Ivan Kopylov <ivan.kopylove@gmail.com>
2014-08-08 14:00:49 +0400

    Сайты, закрытые ботом хостера RedZ, записываются на счёт бота, а не на счёт БХ. Также производится проверка на закрытость.

--- La_cryogenie/commands/Unreported.cs
+++ La_cryogenie/commands/Unreported.cs
@@ -1,10 +1,13 @@
-﻿using SKYPE4COMLib;
+﻿using FishLib;
+using SKYPE4COMLib;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace La_cryogenie
 {
@@ -18,8 +21,53 @@ namespace La_cryogenie
             this.commandArguments = commandArguments_;
         }
 
+        private void checkRedzHostringAutoClosing(DataTable table)
+        {
+            //spike for redz hosting
+            int countOfAutoClosed = new int();
+            foreach (DataRow row in table.Rows)
+            {
+                URLTools url = new URLTools("http://" + row.Field<string>("host"));
+                Browser br = new Browser();
+                
+                switch (url.secondLevelDomain)
+                {
+                    case "hdd1.ru":
+                    case "je1.ru":
+                    case "xclan.ru":
+                        string html;
+                        try
+                        {
+                            html = br.navigate(url.sourceUrl);
+                            File.WriteAllText(url.host + ".txt", html);
+                            if (html.Contains("Red Z Group.<br>"))
+                            {
+                                Sqlite.executeVoid(string.Format("UPDATE [links] SET last_report_to_hoster = '{0}', reporter_to_hoster = '{1}' WHERE host = '{2}' AND (category = 'phishing_page' OR category = 'malware');", Utilities.getCurrentUnixTime(), SkypeSingleton.Instance.skype.CurrentUserHandle, url.host));
+                                countOfAutoClosed++;
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            SkypeSingleton.Instance.sendPrivateMessageBySkypeLogin("ivan.kopilov", "Ошибка в функции checkRedzHostringAutoClosing при загрузке html");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (countOfAutoClosed > 0)
+            {
+                SkypeSingleton.Instance.sendChatMessage(msg.ChatName, string.Format("Сайтов было автоматически закрыто ботом хостера http://redz.ru/: {0}", countOfAutoClosed.ToString()));
+            }
+
+        }
+
         public void postUnreported()
         {
+            DataTable phishingUrlsRedzHostring = Sqlite.executeSearch(string.Format("SELECT * FROM [links] WHERE last_report_to_hoster = 0 AND (category = 'phishing_page' OR category = 'malware');"));
+            checkRedzHostringAutoClosing(phishingUrlsRedzHostring);
+
             DataTable phishingUrls = Sqlite.executeSearch(string.Format("SELECT * FROM [links] WHERE last_report_to_hoster = 0 AND (category = 'phishing_page' OR category = 'malware');"));
             if (phishingUrls.Rows.Count == 0)
             {
